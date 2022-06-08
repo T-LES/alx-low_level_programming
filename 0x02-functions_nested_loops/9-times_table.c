2230c9057eea48d602acda1105d1fd4cc1c0d865
T-LES <lucaselat@gmail.com>
2022-06-08 01:38:44 +0000

    Function to print the 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,9 +10,9 @@ void times_table(void)
 {
 	int i, j;
 
-	for (i = 0; i < 10; i++)
+	for (i = 48; i < 57; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 48; j < 57; j++)
 		{
 			_putchar(i * j);
 			_putchar(44);
