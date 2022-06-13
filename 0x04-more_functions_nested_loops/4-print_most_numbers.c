282b8cc4c638f1f4907495c23e84da9983d8eb80
T-LES <lucaselat@gmail.com>
2022-06-13 09:56:42 +0000

    Prints digit except 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,13 +8,12 @@
  */
 void print_most_numbers(void)
 {
-	int i = 48;
+	int i;
 
-	while ((i != 50 && i != 52))
+	for (i = 48; i < 58; i++)
 	{
-		if (i <= 57)
+		if (i != 50 && i != 52)
 			_putchar(i);
-		i++;
 	}
 	_putchar(10);
 }
