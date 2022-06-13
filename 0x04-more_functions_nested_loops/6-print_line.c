842d055466868ffd13ad3e9fb0f8bf2921a01c2a
T-LES <lucaselat@gmail.com>
2022-06-13 12:12:52 +0000

    Draws a straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ *
+ * @n: is the number of times the character _ should 
+ * be printed
+ *
+ * Return: new line if n is less or equal to 0 and
+ * underscore otherwise 
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i <= 0)
+			_putchar(10);
+		else
+			_putchar(95);
+	}
+	_putchar(10);
+}
