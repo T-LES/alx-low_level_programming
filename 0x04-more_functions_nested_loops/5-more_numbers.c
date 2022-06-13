05346c281d3a07f86e25bd4f854fdd3815c1fb72
T-LES <lucaselat@gmail.com>
2022-06-13 10:55:26 +0000

    Print number

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints ten times the numbers from 0 to 14
+ *
+ * Return: void
+ */
+void print_most_numbers
+{
+	int i, j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i < 15; i++)
+		{
+			if (i < 10)
+				_putchar(i + 48);
+			else
+			{
+				_putchar(49);
+				_putchar((i % 10) + 48);
+			}
+		}
+		_putchar(10);
+	}
+}
