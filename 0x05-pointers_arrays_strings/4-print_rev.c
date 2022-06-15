6f4741ad8c67938ccd5b3d43c307e722b977ce47
T-LES <lucaselat@gmail.com>
2022-06-15 05:04:35 +0000

    How to print a string in reverse order

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,7 +10,7 @@ void print_rev(char *s)
 {
 	int i = 0;
 
-	while (*s != '\0')
+	while (s[i] != '\0')
 	{
 		i++;
 		s++;
@@ -18,7 +18,7 @@ void print_rev(char *s)
 	i = i - 1;
 	while (i >= 0)
 	{
-		_putchar(*s[i]);
+		_putchar(s[i]);
 		i = i - 1;
 	}
 	_putchar(10);
