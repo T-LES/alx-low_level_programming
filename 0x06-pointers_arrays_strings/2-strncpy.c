73241ed8751a7e92aa5248167a1bf2aaee4cbd02
T-LES <lucaselat@gmail.com>
2022-06-22 18:13:44 +0000

    Add a function to copy a string with number of bytes from source

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,21 +4,25 @@
  * *_strncpy - Copies a string
  *
  * @dest: destination string
- * @src: source string 
+ * @src: source string
  * @n: number of bytes
  *
  * Return: a string
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+	int i, j = 0;
+
+	while (dest[j] != '\0')
+		j++;
 
 	for (i = 0; i < n; i++)
 	{
 		if (src[i] == '\0')
 			break;
-		dest[i] = src[i];
+		dest[j] = src[i];
+		j++;
 	}
-	dest[i] = '\0';
+	dest[j] = '\0';
 	return (dest);
 }
