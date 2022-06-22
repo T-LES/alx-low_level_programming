61509b910d2ec173fe23df96faea6f300399febc
T-LES <lucaselat@gmail.com>
2022-06-22 17:13:29 +0000

    Add a function to concatenate two strings with number of bytes from source

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -18,11 +18,10 @@ char *_strncat(char *dest, char *src, int n)
 		i++;
 	for (j = 0; j < n; j++)
 	{
-		while (src[j] != '\0')
-		{
-			dest[i] = src[j];
-			i++;
-		}
+		if (src[j] == '\0')
+			break;
+		dest[i] = src[j];
+		i++;
 	}
 	dest[i] = '\0';
 	return (dest);
