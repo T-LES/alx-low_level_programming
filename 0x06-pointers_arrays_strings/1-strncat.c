b700f33bb8d03eb447fe4f3578f8fe79610d71fe
T-LES <lucaselat@gmail.com>
2022-06-22 16:55:19 +0000

    Add a function to concatenate two strings with number of bytes from source

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ * it will use at most n bytes from source
+ *
+ * @dest: destination string
+ * @src: source string to the destination string
+ *
+ * Return: Resulting string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, j;
+
+	while (dest[i] != '\0')
+		i++;
+	for (j = 0; j < n; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
