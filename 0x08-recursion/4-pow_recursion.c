0badeafb16cb15a118cae37c9ee253a38b2ea890
T-LES <lucaselat@gmail.com>
2022-07-16 20:08:59 +0000

    Adding a function to calculate the power of a number

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,7 +4,7 @@
  * _pow_recursion - returns the value of x
  * raised to the power of y
  * @x: is a number
- * @y is a number
+ * @y: is a number
  * Return: x raised to the power of y
  */
 int _pow_recursion(int x, int y)
