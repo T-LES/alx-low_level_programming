ea180cf9d44d7f2625c6c4c38457a9c42dd48e14
T-LES <lucaselat@gmail.com>
2022-07-17 15:48:39 +0000

    Adding a function to check if a string is a palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,35 @@
+#include "main.h"
+int palindrome(char *str, int start, int end);
+
+/**
+ * palindrome - checks if a string is a palindrome
+ * @str: is a string
+ * @start: is a beginning of the string
+ * @end: is a end of a string
+ * Return: 1 if str is a palindrome and 0 if not
+ */
+int palindrome(char *str, int start, int end)
+{
+	if (start == end)
+		return (1);
+	else if (str[start] != str[end])
+		return (0);
+	else if (start < (end + 1))
+		return (palindrome(str, start + 1, end + 1));
+	return (1);
+}
+
+/**
+ * is_palindrome - returns if a string is a palindrome
+ * @s: is a string
+ * Return: 1 if s is a palindrome or 0 if not
+ */
+int is_palindrome(char *s)
+{
+	int k;
+
+	k = _strlen_recursion(s);
+	if (k == 0)
+		return (1);
+	return (palindrome(s, 0, k - 1));
+}
