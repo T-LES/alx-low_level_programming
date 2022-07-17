69ce3daf3550e858cc090e3ed3689aedbe9c2317
T-LES <lucaselat@gmail.com>
2022-07-17 14:41:54 +0000

    Adding a function to check if one number is prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -5,13 +5,13 @@ int prime(int i, int n);
  * prime - checks if number is prime
  * @i: is a number
  * @n: is a number
- * Return : if n is a prime number 1 or 0 if not
+ * Return: if n is a prime number 1 or 0 if not
  */
 int prime(int i, int n)
 {
 	if (n < 2 || n % i == 0)
-		return 0;
-	else if (i > n)
+		return (0);
+	else if (i > n / 2)
 		return (1);
 	return (prime(i + 1, n));
 }
@@ -20,11 +20,12 @@ int prime(int i, int n)
  * is_prime_number - returns if the input integer
  * is a prime number
  * @n: is a number
- * Return : 1 if n is a prime number or 0 if not
+ * Return: 1 if n is a prime number or 0 if not
  */
 int is_prime_number(int n)
 {
-	int i = 1;
+	int i = 2;
+
 	if (n == 2)
 		return (1);
 	return (prime(i, n));
