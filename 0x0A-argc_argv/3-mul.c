9a724f0399e17404495143fe7ec7a6f87e557048
T-LES <lucaselat@gmail.com>
2022-07-18 19:42:45 +0000

    Fix code

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,14 +11,14 @@ int main(int argc, char *argv[])
 {
 	int res = 1, i;
 
-	if (i > 2)
+	if (argc > 2)
 	{
-		for (i = 1; i < argc, i++)
+		for (i = 1; i < argc; i++)
 			res *= atoi(argv[i]);
 	}
 	else
 	{
-		printf("Error");
+		printf("Error\n");
 		return (1);
 	}
 	printf("%d\n", res);
