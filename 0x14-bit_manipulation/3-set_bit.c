a53424cf832768d0619b94b320c8b386291aa3cc
T-LES <lucaselat@gmail.com>
2022-07-22 17:07:07 +0000

    Adding function to set a bit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * set_bit - set the value of a bit to 1 at a given index.
+ * @n: number to set
+ * @index: index at which to set bit
+ *
+ * Return: 1 if success, or -1 if there is an error
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int k, m;
+
+	m = (sizeof(unsigned long int) * 8);
+	k = 1 >> index;
+
+	if (index >= m)
+		return (-1);
+
+	*n = *n | m;
+	return (1);
+}
