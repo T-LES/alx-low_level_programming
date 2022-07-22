13f87a2478112a16ca011b7e34ec4fbdd466245d
T-LES <lucaselat@gmail.com>
2022-07-22 15:48:25 +0000

    Updating file

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,4 +1,23 @@
 #include "main.h"
+unsigned long int _pow(unsigned int x, unsigned int y);
+
+/**
+ * _pow - calculates (base ^ power)
+ * @x: base of the exponent
+ * @y: power of the exponent
+ *
+ * Return: value of (base ^ power)
+ */
+unsigned long int _pow(unsigned int x, unsigned int y)
+{
+	unsigned long int num;
+	unsigned int i;
+
+	num = 1;
+	for (i = 1; i <= y; i++)
+		num *= x;
+	return (num);
+}
 
 /**
  * print_binary - prints a number in binary notation
@@ -8,7 +27,7 @@
  */
 void print_binary(unsigned long int n)
 {
-	int i = 31, j;
+	int i = 63, j;
 	unsigned long int k;
 
 	if (n == 0)
@@ -16,7 +35,7 @@ void print_binary(unsigned long int n)
 
 	while (i >= 0)
 	{
-		k = 1 << i;
+		k = _pow(2, i);
 
 		if (n >= k)
 		{
